2776a6f9da81c79be6b6334e9e094d480a9e9760
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-11-26 20:50:35 +0300

    Load done

--- ListBox_Lesson/main.cpp
+++ ListBox_Lesson/main.cpp
@@ -1,4 +1,5 @@
-﻿#include<Windows.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<Windows.h>
 #include<Windowsx.h>
 #include"resource.h"
 #include<WinUser.h>
@@ -9,12 +10,13 @@
 #include <magnification.h>
 
 CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first", "List","Box" };//Use Multi-Byte Character Set
-
+CONST CHAR g_sz_FILENAME[] = "list.txt";
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK DlgProcADD(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK DlgProcEDIT(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 VOID SaveList(HWND hwnd, CONST CHAR filename[]);
+VOID LoadList(HWND hwnd, CONST CHAR filename[]);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -28,11 +30,13 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_INITDIALOG:
 	{
-		HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
+		/*HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
 		for (int i = 0; i < sizeof(g_sz_VALUES) / sizeof(g_sz_VALUES[0]); i++)
 		{
 			SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)g_sz_VALUES[i]);
 		}
+		break;*/
+		LoadList(hwnd, g_sz_FILENAME);
 		break;
 	}
 	case WM_COMMAND:
@@ -113,7 +117,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		case IDOK:
 			break;
 		case IDCANCEL:
-			SaveList(hwnd, "list.txt");
+			SaveList(hwnd, g_sz_FILENAME);
 			EndDialog(hwnd, 0);
 		}
 		break;
@@ -156,7 +160,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	//}
 
 	case WM_CLOSE:
-		SaveList(hwnd, "list.txt");
+		SaveList(hwnd, g_sz_FILENAME);
 		EndDialog(hwnd, 0);
 		break;
 	}
@@ -280,4 +284,29 @@ VOID SaveList(HWND hwnd, CONST CHAR filename[])
 	);
 	DWORD dwBytesWritten = 0;
 	WriteFile(hFile, sz_buffer, strlen(sz_buffer) + 1, &dwBytesWritten, NULL);
+}
+VOID LoadList(HWND hwnd, CONST CHAR filename[])
+{
+	HANDLE hFile = CreateFile
+	(
+		filename,
+		GENERIC_READ,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL
+		);
+	DWORD dwError = GetLastError();
+	if (dwError == ERROR_FILE_NOT_FOUND)return;
+	CONST INT SIZE = 32768;
+	CHAR sz_buffer[SIZE] = {};
+	DWORD dwByteRead = 0;
+	ReadFile(hFile, sz_buffer, SIZE, &dwByteRead, NULL);
+	HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+	for (char* pch = strtok(sz_buffer, "\n"); pch; pch = strtok(NULL, "\n"))
+	{
+		SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)pch);
+	}
+	CloseHandle(hFile);
 }

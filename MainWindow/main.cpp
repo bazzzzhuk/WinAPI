e95f2fe516127a58af8f311803327390be42eea8
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-11-28 21:32:40 +0300

    MainWindow done

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,5 +1,6 @@
 ﻿//MainWindow
 #include<Windows.h>
+#include"resource.h"
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My first window";
 
@@ -15,9 +16,19 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wClass.cbClsExtra = 0;
 	wClass.cbWndExtra = 0;
 	//  Инициализируем внешний вид окон
-	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_DOGE));
+	wClass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_VEGAN));
+	//wClass.hIconSm = (HICON)LoadImage(hInstance, "Doge.ico", IMAGE_ICON, LR_DEFAULTSIZE,  LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wClass.hIcon = (HICON)LoadImage(hInstance, "vegan.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wClass.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR1));
+	wClass.hCursor = (HCURSOR)LoadImage
+	(hInstance, 
+		"starcraft-original\\c_f_4.cur",
+		IMAGE_CURSOR,
+		32,
+		32,
+		LR_LOADFROMFILE
+		);
 	wClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	//  инициализация системных переменных:
 	wClass.hInstance = hInstance;
@@ -37,22 +48,30 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		g_sz_WINDOW_CLASS,//Заголовок окна
 		WS_OVERLAPPEDWINDOW,//Стиль окна. Стили всегда зависят от класса окна. WS_OVERLAPPEDWINDOW - главное окно.
 		CW_USEDEFAULT, CW_USEDEFAULT,//Position
-		CW_USEDEFAULT, CW_USEDEFAULT,//size window
+		500, 500,//size window
 		NULL,
 		NULL,//Для главного окна это ResourceID главного меню, для дочернего окна (Control) ResourceID дочернего окна (IDC_BUTTON_COPY)
 		hInstance,
 		NULL
 	);
-	if (hwnd = NULL)
+	if (hwnd == NULL)
 	{
 		MessageBox(NULL, "Window creation failed", NULL, MB_OK | MB_ICONERROR);
 		return 0;
 	}
 
-	ShowWindow(hwnd, nCmdShow);
-	UpdateWindow(hwnd);
+	ShowWindow(hwnd, nCmdShow);//Режим отображения окна - (развернут на весь экран, свёрнуто в окно или же свёрнуто на панель задач)
+	UpdateWindow(hwnd);//Обновляет рабочую область окна отправляя сообщение WM_Paint, если клиентская область окна не пустая.
 
-	return 0;
+	//3. Запуск цикла сообщений.
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)>0)
+	{
+		TranslateMessage(&msg);//Преобразует сообщения виртуальных клавиш в символьные сообщени. 
+		DispatchMessage(&msg); //Отправление сообщение в процедуру окна.
+	}
+
+	return msg.wParam;
 }
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {

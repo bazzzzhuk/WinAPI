eee5791a19f32df07527d07a4356f015e12ab264
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-12-08 20:52:33 +0300

    Calculation done Need check

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -1,6 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<winuser.h>
+#include<wchar.h>
 #include<iostream>
+#include <stdio.h>
 #include"resource.h"
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "Calc PV_521";
@@ -202,7 +205,12 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 	break;
 	case WM_COMMAND:
-	{		
+	{
+		static DOUBLE a = DBL_MIN, b = DBL_MIN;
+		static INT operation = 0;
+		static BOOL input = FALSE;//Срабатывает при нажатии цифр.
+		static BOOL input_operation = FALSE;//Срабатывает при нажатии знака операции.
+
 		SetFocus(hwnd);
 		HWND hEdit = GetDlgItem(hwnd, IDC_DISPLAY);
 		CONST INT SIZE = 256;
@@ -210,11 +218,19 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
 		{
+			if (input_operation)
+			{
+				//SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"0");
+				sz_buffer[0] = 0;
+				input_operation = FALSE;
+			}
 			CHAR sz_digit[2] = {};
 			sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
 			if (strcmp(sz_buffer, "0") == 0)strcpy(sz_buffer, sz_digit);//strcpy - string copy
 			else lstrcat(sz_buffer, sz_digit);//strcat - string concatenation, strcat(dst,src) dst-distanation, src Source
 			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			input = TRUE;
+			input_operation = FALSE;
 		}
 		if (LOWORD(wParam) == IDC_BUTTON_POINT)
 		{
@@ -229,7 +245,41 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 		if (LOWORD(wParam) == IDC_BUTTON_CLR)
 		{
+			a = b = DBL_MIN;
+			operation = 0;
+			input_operation = input = FALSE;
 			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"0");
+		}
+		if (LOWORD(wParam) >= IDC_BUTTON_PLUS && LOWORD(wParam) <= IDC_BUTTON_SLASH)
+		{
+			if (input)
+			{
+				if (a == DBL_MIN)a = atof(sz_buffer);//ANSI/ASCII to double
+				else b = atof(sz_buffer);
+				input = FALSE;
+				SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_EQUAL), 0);
+			}
+			operation = LOWORD(wParam);
+			input_operation = TRUE;
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_EQUAL)
+		{
+			if (input)
+			{
+				if (a == DBL_MIN)a = atof(sz_buffer);
+				else b = atof(sz_buffer);
+				input = FALSE;
+			}
+			switch (operation)
+			{
+			case IDC_BUTTON_PLUS: a += b; break;
+			case IDC_BUTTON_MINUS: a -= b; break;
+			case IDC_BUTTON_ASTER: a *= b; break;
+			case IDC_BUTTON_SLASH: a /= b; break;
+			}
+			input_operation = FALSE;
+			sprintf(sz_buffer, "%g", a);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		}
 	}
 	break;

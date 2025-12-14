2a6235de1913abf6b67032e9eb781115821bcd5d
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-12-15 00:22:22 +0300

    EDIT resource NOT WORK

--- square_blue/resource.h
+++ square_blue/resource.h
@@ -2,34 +2,41 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDC_BUTTON_0			1000
-#define IDC_BUTTON_1			1001
-#define IDC_BUTTON_2			1002
-#define IDC_BUTTON_3			1003
-#define IDC_BUTTON_4			1004
-#define IDC_BUTTON_5			1005
-#define IDC_BUTTON_6			1006
-#define IDC_BUTTON_7			1007
-#define IDC_BUTTON_8			1008
-#define IDC_BUTTON_9			1009
-//--------------------------------//
-#define IDC_BUTTON_POINT		1010
-#define IDC_BUTTON_PLUS			1011
-#define IDC_BUTTON_MINUS		1012
-#define IDC_BUTTON_ASTER /* * */1013
-#define IDC_BUTTON_SLASH		1014
-//--------------------------------//
-#define IDC_BUTTON_BSP			1015 // Backspace
-#define IDC_BUTTON_CLR			1016 // Clear
-#define IDC_BUTTON_EQUAL		1017 // =
+#define IDB_BITMAP_POINT                120
+#define IDB_BITMAP_PLUS                 121
+#define IDB_BITMAP_MINUS                122
+#define IDB_BITMAP_ASTER                123
+#define IDB_BITMAP_SLASH                124
+#define IDB_BITMAP_BSP                  125
+#define IDB_BITMAP_CLR                  126
+#define IDB_BITMAP1                     128
+#define IDB_BITMAP_EQUAL                128
+#define IDC_BUTTON_0                    1000
+#define IDC_BUTTON_1                    1001
+#define IDC_BUTTON_2                    1002
+#define IDC_BUTTON_3                    1003
+#define IDC_BUTTON_4                    1004
+#define IDC_BUTTON_5                    1005
+#define IDC_BUTTON_6                    1006
+#define IDC_BUTTON_7                    1007
+#define IDC_BUTTON_8                    1008
+#define IDC_BUTTON_9                    1009
+#define IDC_BUTTON_POINT                1010
+#define IDC_BUTTON_PLUS                 1011
+#define IDC_BUTTON_MINUS                1012
+#define IDC_BUTTON_ASTER                1013
+#define IDC_BUTTON_SLASH                1014
+#define IDC_BUTTON_BSP                  1015
+#define IDC_BUTTON_CLR                  1016
+#define IDC_BUTTON_EQUAL                1017
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        120
+#define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_SYMED_VALUE           109
 #endif
 #endif

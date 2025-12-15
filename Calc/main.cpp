679992de1417bd74befd0e26290862e46cfb2614
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-12-15 19:07:09 +0300

    Yuoohooo done

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -545,7 +545,7 @@ VOID SetSkinDLL(HWND hwnd, CONST CHAR skin[])
 {
 	CHAR sz_filename[FILENAME_MAX] = {};
 	sprintf(sz_filename, "..\\%s\\.", skin);
-	HINSTANCE hSkin = LoadLibrary(skin);
+	HINSTANCE hSkin = LoadLibrary("square_blue.dll");
 	for (int i = IDC_BUTTON_0; i <= IDC_BUTTON_EQUAL; i++)
 	{
 		HBITMAP hImageLoad = (HBITMAP)LoadImage
--- square_blue/resource.h
+++ square_blue/resource.h
@@ -2,24 +2,24 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDC_BUTTON_0                    1000
-#define IDC_BUTTON_1                    1001
-#define IDC_BUTTON_2                    1002
-#define IDC_BUTTON_3                    1003
-#define IDC_BUTTON_4                    1004
-#define IDC_BUTTON_5                    1005
-#define IDC_BUTTON_6                    1006
-#define IDC_BUTTON_7                    1007
-#define IDC_BUTTON_8                    1008
-#define IDC_BUTTON_9                    1009
-#define IDC_BUTTON_POINT                1010
-#define IDC_BUTTON_PLUS                 1011
-#define IDC_BUTTON_MINUS                1012
-#define IDC_BUTTON_ASTER                1013
-#define IDC_BUTTON_SLASH                1014
-#define IDC_BUTTON_BSP                  1015
-#define IDC_BUTTON_CLR                  1016
-#define IDC_BUTTON_EQUAL                1017
+#define IDB_BUTTON_0                    1000
+#define IDB_BUTTON_1                    1001
+#define IDB_BUTTON_2                    1002
+#define IDB_BUTTON_3                    1003
+#define IDB_BUTTON_4                    1004
+#define IDB_BUTTON_5                    1005
+#define IDB_BUTTON_6                    1006
+#define IDB_BUTTON_7                    1007
+#define IDB_BUTTON_8                    1008
+#define IDB_BUTTON_9                    1009
+#define IDB_BUTTON_POINT                1010
+#define IDB_BUTTON_PLUS                 1011
+#define IDB_BUTTON_MINUS                1012
+#define IDB_BUTTON_ASTER                1013
+#define IDB_BUTTON_SLASH                1014
+#define IDB_BUTTON_BSP                  1015
+#define IDB_BUTTON_CLR                  1016
+#define IDB_BUTTON_EQUAL                1017
 
 // Next default values for new objects
 // 
